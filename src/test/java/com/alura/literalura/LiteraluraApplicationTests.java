641e27de2ed8fe3a254375c6a9b721b4ebfdffeb
luisssuki <67665604+luisssuki@users.noreply.github.com>
2024-07-16 17:12:31 -0600

    Add files via upload

---
+++ src/test/java/com/alura/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,12 @@
+package com.alura.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
